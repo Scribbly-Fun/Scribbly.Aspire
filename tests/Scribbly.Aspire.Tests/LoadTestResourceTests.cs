5bb507457ad894dc6b2bfb792e57d58dbf0daf44
Eric Williams <ewilliams0305@gmail.com>
2025-11-04 13:52:20 -0500

    Update LoadTestResourceTests.cs

--- tests/Scribbly.Aspire.Tests/LoadTestResourceTests.cs
+++ tests/Scribbly.Aspire.Tests/LoadTestResourceTests.cs
@@ -11,18 +11,16 @@ public class WebTests
         {
             clientBuilder.AddStandardResilienceHandler();
         });
-        // To output logs to the xUnit.net ITestOutputHelper, consider adding a package from https://www.nuget.org/packages?q=xunit+logging
-
+        
         await using var app = await appHost.BuildAsync();
         var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
         await app.StartAsync();
 
         // Act
-        var httpClient = app.CreateHttpClient("apiservice");
-        await resourceNotificationService.WaitForResourceAsync("apiservice", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
-        var response = await httpClient.GetAsync("/weatherforecast");
+        
+        await resourceNotificationService.WaitForResourceAsync("load-test", KnownResourceStates.Waiting).WaitAsync(TimeSpan.FromSeconds(30));
 
         // Assert
-        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.True(true);
     }
 }
